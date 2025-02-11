e4d175da917d561f7999a580f80145cf1a37d213
diegorezm <dmourarezende@gmail.com>
2025-02-10 21:59:54 -0300

    find job preferences

--- src/main/java/com/rmtjb/api/repositories/JobPostingRepository.java
+++ src/main/java/com/rmtjb/api/repositories/JobPostingRepository.java
@@ -5,10 +5,15 @@ import com.rmtjb.api.domain.job.JobPosting;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 /** JobPostingRepository */
 public interface JobPostingRepository extends JpaRepository<JobPosting, UUID> {
-    Page<JobPosting> findAll(Pageable pageable);
+    @Query(
+            value = "SELECT * FROM rmtjbs_job_postings j WHERE :preference = ANY (j.skills)",
+            nativeQuery = true)
+    Page<JobPosting> findByPreferences(@Param("preference") String preference, Pageable pageable);
 }
