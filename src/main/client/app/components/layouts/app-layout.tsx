11895b2cef27c10645547d461b124f85b32e44bf
diegorezm <dmourarezende@gmail.com>
2025-02-20 16:29:59 -0300

    no need for this code

--- src/main/client/app/components/layouts/app-layout.tsx
+++ src/main/client/app/components/layouts/app-layout.tsx
@@ -2,7 +2,6 @@ import { QueryProvider } from "~/providers/query-provider";
 import { Navbar } from "../navbar";
 import { Outlet } from "react-router";
 import { AuthProvider } from "~/providers/auth-provider";
-import { SocketClientProvider } from "~/providers/websocket-provider";
 
 export default function AppLayout() {
   return (
